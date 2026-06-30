db5408dc99e9fdc100e0de08f9263aaf0e645946
Petras Šukys <petras@shoekeys.lt>
2026-06-30 10:27:13 +0300

    run/stop script updates

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 -m http.server 8000 --bind 127.0.0.1 & sleep 2 && xdg-open http://localhost:8000 || (curl -s "http://localhost:8000" > /dev/null)
+python3 -m http.server 8000 --bind 127.0.0.1 & echo $! > server.pid && sleep 2 && xdg-open http://localhost:8000 || (curl -s "http://localhost:8000" > /dev/null)
